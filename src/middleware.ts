717f8220616f5e3c68ab3bb1e3b31c6f4e6bed19
SREEHARI M.S. <30307421+sreeharims98@users.noreply.github.com>
2025-05-18 11:36:56 +0530

    fix build and add middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-// import { createServerClient } from "@supabase/ssr";
+import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
@@ -12,38 +12,80 @@ export const config = {
 };
 
 export async function updateSession(request: NextRequest) {
-  const supabaseResponse = NextResponse.next({
+  let supabaseResponse = NextResponse.next({
     request,
   });
 
-  console.log("middleware");
+  const supabase = createServerClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value),
+          );
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options),
+          );
+        },
+      },
+    },
+  );
 
-  //   const supabase = createServerClient(
-  //     process.env.SUPABASE_URL!,
-  //     process.env.SUPABASE_ANON_KEY!,
-  //     {
-  //       cookies: {
-  //         getAll() {
-  //           return request.cookies.getAll();
-  //         },
-  //         setAll(cookiesToSet) {
-  //           cookiesToSet.forEach(({ name, value }) =>
-  //             request.cookies.set(name, value),
-  //           );
-  //           supabaseResponse = NextResponse.next({
-  //             request,
-  //           });
-  //           cookiesToSet.forEach(({ name, value, options }) =>
-  //             supabaseResponse.cookies.set(name, value, options),
-  //           );
-  //         },
-  //       },
-  //     },
-  //   );
+  const isAuthRoute =
+    request.nextUrl.pathname === "/login" ||
+    request.nextUrl.pathname === "/sign-up";
 
-  //   const {
-  //     data: { user },
-  //   } = await supabase.auth.getUser();
+  if (isAuthRoute) {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (user) {
+      return NextResponse.redirect(
+        new URL("/", process.env.NEXT_PUBLIC_BASE_URL),
+      );
+    }
+  }
+
+  const { searchParams, pathname } = new URL(request.url);
+
+  if (!searchParams.get("noteId") && pathname === "/") {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    if (user) {
+      const { newestNoteId } = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/fetch-newest-note?userId=${user.id}`,
+      ).then((res) => res.json());
+
+      if (newestNoteId) {
+        const url = request.nextUrl.clone();
+        url.searchParams.set("noteId", newestNoteId);
+        return NextResponse.redirect(url);
+      } else {
+        const { noteId } = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/create-new-note?userId=${user.id}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          },
+        ).then((res) => res.json());
+        const url = request.nextUrl.clone();
+        url.searchParams.set("noteId", noteId);
+        return NextResponse.redirect(url);
+      }
+    }
+  }
 
   return supabaseResponse;
 }
